9d36a14f33953100f4447d245ba29b4ff54187e9
Bill Justin <legn.tbj@gmail.com>
2025-03-19 11:16:26 +0800

    updated link element, now appending to light DOM instad of shadow DOM

--- link-with-pardot-inside.js
+++ link-with-pardot-inside.js
@@ -2,21 +2,32 @@ class LinkWithPardotInside extends HTMLElement {
     constructor() {
         super();
         this.handleProps();
+        this.render()
     }
 
     handleProps() {
         this.hrefUrl = this.getAttribute("href-url") || "#";
-        this.pardotRegion = this.getAttribute("pardot-region")
+        this.pardotRegion = this.getAttribute("pardot-region") || "";
+        this.text = this.getAttribute("text")
     }
 
-    connectedCallback() {
-        this.innerHTML = `
-            <a pardot-region="${this.pardotRegion}"
-                href="${this.hrefUrl}"
-            >
-                <slot></slot>
-            </a>
-        `
+
+    render() {
+        // const wrapper = document.createElement("div");
+
+        // wrapper.innerHTML = `
+        //     <a href="${this.hrefUrl}">
+        //         ${this.text}
+        //     </a>
+        // `
+        // this.appendChild(wrapper)
+
+        const link = document.createElement("a")
+        link.setAttribute("href", this.hrefUrl)
+        link.setAttribute("pardot-region", this.pardotRegion)
+        link.textContent = this.text
+
+        this.appendChild(link)
     }
 }
 
